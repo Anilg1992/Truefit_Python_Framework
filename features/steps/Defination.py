9538452a56fadfe850fc38842d34cf1242a26302
AGopu <anilreddyde@gmail.com>
2018-10-24 10:35:41 -0400

    "Committing the new log files for python bdd framework"

--- features/steps/Defination.py
+++ features/steps/Defination.py
@@ -35,39 +35,47 @@ class Defination(unittest.TestCase):
 
     @Then("I am making sure google logo is displayed")
     def I_am_making_sure_google_logo_is_displayed(context):
+        print("Verifying google logo is present")
         if (d.isElementPresent("//img[@id='hplogo']", locatorType='xpath') != True):
             d.screenShot("I_am_making_sure_google_logo_is_displayed")
 
     @Then("I am making sure that search box is present")
     def I_am_making_sure_that_search_box_is_present(context):
+        print("Verifying for search box present")
         if (d.isElementPresent("//input[@title='Search']", locatorType='xpath') != True):
             d.screenShot("I_am_making_sure_that_search_box_is_present")
             fail("I am making sure that search box is present")
 
     @Then("I am making sure that search button is present")
     def I_am_making_sure_that_search_button_is_present(context):
+        print("checking for search button present")
         if (d.isElementPresent("//input[@name='btnK']", locatorType='xpath') != True):
             d.screenShot("I_am_making_sure_that_search_button_is_present")
             fail("I am making sure that search button is present")
 
     @When("I am clicking search button without entering anything on textbox")
     def I_am_clicking_search_button_without_entering_anything_on_textbox(context):
+        print("Without text clicking google search")
         d.elementClick("//input[@name='btnK']", locatorType='xpath')
 
     @Then("I am still on the google home page and no search results are displayed")
     def I_am_still_on_the_google_home_page_and_no_search_results_are_displayed(context):
+        print("Results are not displayed")
         if (d.isElementPresent("//input[@name='btnI']", locatorType='xpath') != True):
             d.screenShot("I_am_still_on_the_google_home_page_and_no_search_results_are_displayed")
             fail("I am still on the google home page and no search results are displayed")
 
     @When("I am clicking search button by entering \"{input}\" on textbox")
     def I_am_clicking_search_button_by_entering(context, input):
+        print("Sending text into the search box")
         d.sendKeys(input, "//input[@title='Search']", locatorType='xpath')
         time.sleep(10)
+        print("After Sending text into the search box clicking google search")
         d.elementClick("//input[@value='Google Search']", locatorType='xpath')
 
     @Then("I am making sure that I am getting the search results")
     def I_am_making_sure_that_I_am_getting_the_search_results(context):
+        print("Search results displayed")
         time.sleep(10)
         if (d.isElementPresent("(//a[contains(@href,'truefit')])[1]", locatorType='xpath') != True):
             d.screenShot('I_am_making_sure_that_I_am_getting_the_search_results')
